58df26868dfd374ae8fdfc44d59f37f7fdb009c1
sudarat58160643 <53961025+sudarat58160643@users.noreply.github.com>
2019-09-28 10:48:13 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.json())
-let todo =[]
-app.get('/todo',(req,res) =>{
-    res.send({message:'Hello todo'})
-})
 
-app.get('/todo',(req,res) =>{
+
+app.use(express.json())
+
+
+let todos =[
+    { id: 1,title : 'simple todo', completed: false},
+    { id: 2,title : 'Completed sample todo', completed: true},
+]
+app.get('/todos',(req,res) =>{
     res.send(todo)
 })
+ 
+//list todo
+app.post('/todos',(req,res)=>{
+    let title = req.body.title
+    let todo = {title: title,completed:false,id:todos.length}
+    todos.push(todo)
+    res.sendStatus(201).send(todo)
+})
+
+
+
+app.get('/todos',(req,res) =>{
+    
+let id = req.params.id
+let result = todos.filter(todo => todo.id.toString() === id)
+let todo = result[0]
+res.send(todo)
+
+   
+})
+
+
+
 
 app.post('/todo',(req, res) =>{
     let name = req.body.name
@@ -22,5 +48,5 @@ app.post('/todo',(req, res) =>{
 
 
 app.listen(port,()=>{
-    console.log('Welcome to Todos ' + port)
+    console.log('Todo-API started ' + port)
 })
