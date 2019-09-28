bf27a65f122e622f1c36ad92c265e8ba1da6e1ce
sudarat58160643 <53961025+sudarat58160643@users.noreply.github.com>
2019-09-28 11:34:29 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
 app.use(express.json())
-
 
 let todos =[
     { id: 1,title : 'simple todo', completed: false},
@@ -22,30 +20,36 @@ app.post('/todos',(req,res)=>{
     res.sendStatus(201).send(todo)
 })
 
-
-
 app.get('/todos',(req,res) =>{
     
 let id = req.params.id
 let result = todos.filter(todo => todo.id.toString() === id)
 let todo = result[0]
 res.send(todo)
-
-   
+ 
 })
 
-
-
-
-app.post('/todo',(req, res) =>{
+app.post('/todos',(req, res) =>{
     let name = req.body.name
     let p = {name:name}
     todo.push(p)
     res.sendStatus(201)
 })
 
+app.delete('/todo/:id',(req,res)=>{
+    let id = req.param.id
+    delete todos[id-1]
+    let result = todos.filter(todo => todo.id.toString() !== id
+    todos = result
+    res.sendStatus(204)
+})
 
 
+app.put('/todo/:id',(req,res)=>{
+    let id = req.param.id
+    let todo = todos.find(t => t.id.toString() === id
+    res.send(todo)
+})
 
 app.listen(port,()=>{
     console.log('Todo-API started ' + port)
